f851634c0f48f0be5be68b205c60eded46d99bda
HungT <64987549+Winnie1996@users.noreply.github.com>
2020-09-04 08:56:37 -0700

    Added Stuff

--- components/Carousel.js
+++ components/Carousel.js
@@ -1,4 +1,3 @@
-import axios from "axios";
 /*
   STRETCH GOAL
   STRETCH GOAL
@@ -23,62 +22,94 @@ import axios from "axios";
     <div class="right-button"> > </div>
   </div>
 */
+//Doesn't make sense to put it where the HTML says to put it.
+const carouselCont = document.querySelector(".header-container");
 
-function constructor() {
-  const imageObj = {
-    0: "./assets/carousel/mountains.jpeg",
-    1: "./assets/carousel/computer.jpeg",
-    2: "./assets/carousel/trees.jpeg",
-    3: "./assets/carousel/turntable.jpeg",
+const carouselCreator = () => {
+  //Making Elements
+  const carousel = document.createElement("div"),
+    leftButton = document.createElement("div"),
+    imageOne = document.createElement("img"),
+    imageTwo = document.createElement("img"),
+    imageThree = document.createElement("img"),
+    imageFour = document.createElement("img"),
+    rightButton = document.createElement("div");
+
+  //Adding classes and src to images
+  carousel.classList.add("carousel");
+  leftButton.classList.add("left-button");
+  imageOne.src =
+    "https://tk-assets.lambdaschool.com/8aa075b0-67cf-47ce-9a7f-8cc9d754675d_computer.jpeg";
+  imageTwo.src =
+    "https://tk-assets.lambdaschool.com/ba687af4-3a1e-43d7-87b2-f30453264c9d_mountains.jpeg";
+  imageThree.src =
+    "https://tk-assets.lambdaschool.com/0b770382-d0eb-4465-8bf2-692a79fcda71_turntable.jpeg";
+  imageFour.src =
+    "https://tk-assets.lambdaschool.com/5b7441c6-6e4b-4feb-a4ec-8dd2eb76238a_trees.jpeg";
+  rightButton.classList.add("right-button");
+
+  //Appending
+  carousel.appendChild(leftButton);
+  carousel.appendChild(imageOne);
+  carousel.appendChild(imageTwo);
+  carousel.appendChild(imageThree);
+  carousel.appendChild(imageFour);
+  carousel.appendChild(rightButton);
+
+  //events for buttons
+  let slideIndex = 0;
+
+  const showSlides = (number) => {
+    slideIndex += number;
+
+    if (slideIndex >= 5) {
+      slideIndex = 1;
+    }
+
+    if (slideIndex <= 0) {
+      slideIndex = 4;
+    }
+
+    if (slideIndex === 1) {
+      imageOne.style.display = "block";
+      imageTwo.style.display = "none";
+      imageThree.style.display = "none";
+      imageFour.style.display = "none";
+    } else if (slideIndex === 2) {
+      imageTwo.style.display = "block";
+      imageOne.style.display = "none";
+      imageThree.style.display = "none";
+      imageFour.style.display = "none";
+    } else if (slideIndex === 3) {
+      imageThree.style.display = "block";
+      imageOne.style.display = "none";
+      imageTwo.style.display = "none";
+      imageFour.style.display = "none";
+    } else if (slideIndex === 4) {
+      imageFour.style.display = "block";
+      imageOne.style.display = "none";
+      imageTwo.style.display = "none";
+      imageThree.style.display = "none";
+    }
+
+    return carousel;
   };
 
-  let imageIndex = 0;
+  showSlides(1);
 
-  const carousel = document.createElement("div");
-  const leftButton = document.createElement("div");
-  leftButton.classList.add("left-button");
-  const rightButton = document.createElement("div");
-  rightButton.classList.add("right-button");
-  const caroImg = document.createElement("img");
+  // leftButton.addEventListener('click', () => {
+  //   showSlides(-1);
+  // });
 
-  //classes
-  carousel.classList.add("carousel");
-  // leftButton.textContent = "left";
-  // rightButton.textContent = "right";
+  leftButton.addEventListener("click", () => {
+    showSlides(-1);
+  });
 
-  caroImg.src = imageObj[imageIndex];
-  function increment() {
-    console.log("+");
-    if (imageIndex === 3) {
-      imageIndex = 0;
-    } else {
-      imageIndex = imageIndex + 1;
-    }
-    caroImg.src = imageObj[imageIndex];
-  }
+  rightButton.addEventListener("click", () => {
+    showSlides(1);
+  });
 
-  function decrement() {
-    console.log("-");
-    if (imageIndex === 0) {
-      imageIndex = 3;
-    } else {
-      imageIndex = imageIndex - 1;
-    }
-    caroImg.src = imageObj[imageIndex];
-  }
-
-  leftButton.onclick = decrement;
-  rightButton.onclick = increment;
-  //src
-
-  //append
-  carousel.append(caroImg, leftButton, rightButton);
-
-  //return
   return carousel;
-}
+};
 
-const entryPoint = document.querySelector(".carousel-container");
-const caro = constructor();
-// entryPoint.append(constructor());
-entryPoint.appendChild(caro);
+carouselCont.appendChild(carouselCreator());
