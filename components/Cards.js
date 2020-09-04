5b2b6ef08d11ad36f158fda2eeb4c092635fad2f
HungT <64987549+Winnie1996@users.noreply.github.com>
2020-09-04 08:40:50 -0700

    MET MVP Trying to do Sprint Challenge

--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,56 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+
+const cardContainer = document.querySelector(".cards-container");
+
+// Create Elements
+
+const cardCreator = function (data) {
+  const parentCardDiv = document.createElement("div");
+  const headerTitle = document.createElement("div");
+  const author = document.createElement("div");
+  const imgDiv = document.createElement("div");
+  const image = document.createElement("img");
+  const name = document.createElement("span");
+
+  // Add Classes
+  parentCardDiv.classList.add("card");
+  headerTitle.classList.add("headline");
+  author.classList.add("author");
+  imgDiv.classList.add("img-container");
+  image.src = data.authorPhoto;
+
+  // Text Content
+  headerTitle.textContent = data.headline;
+  name.textContent = data.authorName;
+
+  //Append
+  parentCardDiv.appendChild(headerTitle);
+  parentCardDiv.appendChild(author);
+  author.appendChild(imgDiv);
+  imgDiv.appendChild(image);
+  author.appendChild(name);
+
+  return parentCardDiv;
+};
+
+axios
+  .get("https://lambda-times-api.herokuapp.com/articles")
+  .then(function (response) {
+    console.log(response);
+    //was an object and not an array
+
+    const newerArticles = Object.values(response.data.articles);
+
+    newerArticles.forEach(function (item) {
+      item.forEach(function (extraItems) {
+        const newArticles = cardCreator(extraItems);
+        cardContainer.appendChild(newArticles);
+      });
+    });
+  })
+
+  .catch(function (error) {
+    console.log(`WOOPS CARD ERROR BUD: ${error}`);
+  });
